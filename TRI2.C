c4d551765af4c0883f56c53b0fec3eb424871ee6
alamiljoseph <31540501+alamiljoseph@users.noreply.github.com>
2020-10-17 16:27:29 +0800

    Initial

---
+++ TRI2.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+{
+  int i,j, k;
+  clrscr();
+
+  for(i = 0; i < 24; i++);
+  {
+     for(j = i; j >= 0; j--);
+     {
+	printf(" ");
+     }
+     for(k = 0; k <= i; k++)
+       printf("*");
+     printf("\n");
+  }
+
+  getch();
+}
