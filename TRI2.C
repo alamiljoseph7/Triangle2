a4304cdef62cfd6a395c6c93e3e9c89c3b0054eb
jhonjhezryll69420 <72652672+jhonjhezryll69420@users.noreply.github.com>
2020-10-17 18:22:20 +0800

    Update TRI2.C

--- TRI2.C
+++ TRI2.C
@@ -2,17 +2,19 @@
 
 void main()
 {
-  int i,j, k;
+  int i, j, k;
   clrscr();
 
-  for(i = 0; i < 24; i++);
+  for(i = 0; i < 24; i++)
   {
-     for(j = i; j >= 0; j--);
+     for(j = 24 - i; j >= 0; j--)
      {
 	printf(" ");
      }
      for(k = 0; k <= i; k++)
-       printf("*");
+     {
+        printf("*");
+     } 
      printf("\n");
   }
 
